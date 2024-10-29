190ca25515e6924fa787a3771d56866ab40a88fd
samadesan <samadesan@alu.edu.gva.es>
2024-10-29 12:50:10 +0100

    Duplicados y Comunes

---
+++ src/main/Comunes.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+public class Comunes {
+    public static String comunes(int[] nums1, int[] nums2) {
+        String resultado = "";
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    resultado += nums1[i] + " ";
+                    break;
+                }
+            }
+        }
+        return resultado;
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {4, 5, 7, 9, 4, 2};
+        int[] nums2 = {4, 6, 2};
+        String resultado = comunes(nums1, nums2);
+        System.out.println(resultado);
+    }
+}
---
+++ src/main/Duplicados.java
@@ -0,0 +1,7 @@
+///2. Duplicados: Escribid un programa Java para encontrar los valores duplicados de un array de valores enteros.
+public class Duplicados {
+    public static String D
+    public static void main(String[] args) {
+
+    }
+}
