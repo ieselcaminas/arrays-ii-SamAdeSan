ebe1d6496a338ad7a6fe90f118d5d9b608577712
samadesan <samadesan@alu.edu.gva.es>
2024-11-05 13:17:35 +0100

    MoverCeros.java

---
+++ src/main/MoverCeros.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+/// 6. MoverCeros: Escribid un programa Java para mover todos los 0 al final de un array. Mantened el orden relativo de los otros elementos de la array (distintos de cero).
+public class MoverCeros {
+    public static void main(String[] args) {
+
+    }
+}
